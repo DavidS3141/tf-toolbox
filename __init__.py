bbc1080c5a2e40e0f401eb6f4d722e4996e41b70
David Schmidt <david.schmidt@rwth-aachen.de>
2018-04-08 00:30:10 +0200

    add summary collector for tf graphs

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
 from .convergence_checker import Convergence_Checker
 from .batch_generator import batch_generator
 from .write_tb_summary import write_tb_summary
+from .create_summaries_on_graph import create_summaries_on_graph
---
+++ create_summaries_on_graph.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+
+
+def create_summaries_on_graph(verbosity):
+    v0_summaries = tf.summary.merge_all('v0')
+    v1_summaries = tf.summary.merge_all('v1')
+    v2_summaries = tf.summary.merge_all('v2')
+    watch_summaries = tf.summary.merge_all('watch')
+    if verbosity == 0:
+        smaller_used_summaries = v0_summaries
+        bigger_used_summaries = v0_summaries
+    elif verbosity == 1:
+        smaller_used_summaries = v0_summaries
+        bigger_used_summaries = v1_summaries
+    elif verbosity == 2:
+        smaller_used_summaries = v1_summaries
+        bigger_used_summaries = v2_summaries
+    elif verbosity >= 3:
+        smaller_used_summaries = v2_summaries
+        bigger_used_summaries = v2_summaries
+    else:
+        raise ValueError('Verbosity has to be a non-negative number instead '
+                         'of %d!' % verbosity)
+    if watch_summaries is not None:
+        smaller_used_summaries = tf.summary.merge([smaller_used_summaries,
+                                                   watch_summaries])
+        bigger_used_summaries = tf.summary.merge([bigger_used_summaries,
+                                                  watch_summaries])
+    # check that all summaries are sorted into one of the verbosity groups
+    all_summaries = tf.summary.merge_all()
+    assert(all_summaries is None)
+    # name the tf_summaries
+    tf.identity(smaller_used_summaries, name='smaller_used_summaries')
+    tf.identity(bigger_used_summaries, name='bigger_used_summaries')
