e501df66eedb35493eb8ade5eb265ac243f85e4c
David Schmidt <david.schmidt@rwth-aachen.de>
2018-04-07 20:52:38 +0200

    add fully connected layer constructor

---
+++ nets/fc.py
@@ -0,0 +1,117 @@
+from .tf_activations import get_activation
+
+import tensorflow as tf
+
+
+def fc_network(layer_sizes, name, in_mean=0., in_std=1., act_name='relu'):
+    with tf.name_scope(name + '_vars'):
+        # create all variables for this network
+        layer_vars = []
+        layer_funcs = []
+        for i in range(len(layer_sizes) - 2):
+            layer_func, layer_var, in_mean, in_std = \
+                fc_layer(layer_sizes[i], layer_sizes[i + 1], 'layer-%d' % (i+1),
+                         act_name=act_name, in_mean=in_mean, in_std=in_std)
+            layer_vars += layer_var
+            layer_funcs.append(layer_func)
+        output, layer_var, out_mean, out_std = \
+            fc_layer(layer_sizes[-2], layer_sizes[-1],
+                     'layer-%d' % (len(layer_sizes) - 1), act_name='identity',
+                     in_mean=in_mean, in_std=in_std)
+        assert out_mean == 0.0
+        assert out_std == 1.0
+        layer_vars += layer_var
+        layer_funcs.append(output)
+
+    # apply network on input
+    def network(x):
+        with tf.name_scope(name):
+            for i in range(len(layer_funcs)):
+                x = layer_funcs[i](x)
+            return x
+
+    return network, layer_vars
+
+
+def fc_layer(input_dim, output_dim, layer_name, act_name, in_mean, in_std):
+    '''
+    Reusable code for making a simple neural net layer.
+
+    It does a matrix multiply, bias add, and then uses act to nonlinearize.
+    It also sets up name scoping so that the resultant graph is easy to read,
+    and adds a number of summary ops.
+    '''
+    # get activation function
+    act, pre_std, post_mean = get_activation(act_name)
+    if act is None:
+        raise Exception('You have to specify a valid activation function! %s '
+                        'is not a valid activation function!' % act_name)
+    # compute weights std scaling
+    weights_std = pre_std**2 / (in_mean**2 + in_std**2)
+    # Adding a name scope ensures logical grouping of the layers in the graph.
+    with tf.name_scope(layer_name + '_vars'):
+        # This Variable will hold the state of the weights for the layer
+        with tf.name_scope('weights'):
+            weights = weight_variable([input_dim, output_dim], weights_std)
+            variable_summaries(weights)
+        with tf.name_scope('biases'):
+            biases = bias_variable([output_dim])
+            variable_summaries(biases)
+
+    def layer(input_tensor):
+        with tf.name_scope(layer_name):
+            with tf.name_scope('Wx_plus_b'):
+                preactivate = tf.matmul(input_tensor, weights) + biases
+                tf.summary.histogram('pre_activations', preactivate,
+                                     collections=['v2'])
+            if act == 'identity':
+                activations = preactivate
+            else:
+                activations = act(preactivate, name='activation')
+            tf.summary.histogram('activations', activations,
+                                 collections=['v2'])
+            tf.summary.scalar('activations/std',
+                              tf.sqrt(tf.reduce_mean(tf.square(
+                                  activations - tf.reduce_mean(activations)))),
+                              collections=['v2', 'watch'])
+            tf.summary.histogram('activations/std_featurewise',
+                                 tf.sqrt(tf.reduce_mean(tf.square(
+                                     activations -
+                                     tf.reshape(tf.reduce_mean(activations, 0),
+                                                [1, -1])), 0)),
+                                 collections=['v2'])
+            return activations
+    return layer, [weights, biases], post_mean, 1.0
+
+
+def weight_variable(dims, weights_std):
+    return tf.Variable(xavier_init(dims, weights_std),
+                       collections=[tf.GraphKeys.GLOBAL_VARIABLES,
+                                    tf.GraphKeys.WEIGHTS])
+
+
+def bias_variable(dims):
+    return tf.Variable(tf.zeros(shape=dims),
+                       collections=[tf.GraphKeys.GLOBAL_VARIABLES,
+                                    tf.GraphKeys.BIASES])
+
+
+def xavier_init(size, weights_std):
+    in_dim = size[0]
+    xavier_stddev = tf.sqrt(weights_std / in_dim)
+    return tf.random_normal(shape=size, stddev=xavier_stddev)
+
+
+def variable_summaries(var):
+    '''
+    Attach a lot of summaries to a Tensor (for TensorBoard visualization).
+    '''
+    with tf.name_scope('summaries'):
+        mean = tf.reduce_mean(var)
+        tf.summary.scalar('mean', mean, collections=['v2'])
+        with tf.name_scope('std'):
+            stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+        tf.summary.scalar('std', stddev, collections=['v2'])
+        tf.summary.scalar('max', tf.reduce_max(var), collections=['v2'])
+        tf.summary.scalar('min', tf.reduce_min(var), collections=['v2'])
+        tf.summary.histogram('histogram', var, collections=['v2'])
---
+++ nets/tf_activations.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+import tensorflow as tf
+
+
+def leaky_relu(x, alpha=0.2, name='leaky relu'):
+    with tf.name_scope(name):
+        return tf.maximum(x, x * alpha)
+
+
+def selu(x, name='selu'):
+    with tf.name_scope(name):
+        return 1.7580993408473768599402175208123 \
+            * (tf.exp(tf.minimum(x, tf.zeros_like(x))) - tf.ones_like(x)) \
+            + 1.0507009873554804934193349852946 * tf.nn.relu(x)
+
+
+def sigmoid_num(x, name='sigmoid num'):
+    with tf.name_scope(name):
+        return 2.8 * tf.nn.sigmoid(x)
+
+
+def softsign_num(x, name='softsign num'):
+    with tf.name_scope(name):
+        return 2 * tf.nn.softsign(x)
+
+
+def tanh_num(x, name='tanh num'):
+    with tf.name_scope(name):
+        return 1.5 * tf.nn.tanh(x)
+
+
+act_name_to_act_tuple = {
+    'elu': (tf.nn.elu, 1., 0.),
+    'elu_num': (tf.nn.elu, 1.321939, 0.250422),
+    'leaky_relu': (leaky_relu, 1.546460, 0.493559),
+    'relu': (tf.nn.relu, 1.712859, 0.683332),
+    'relu6': (tf.nn.relu6, 1.713838, 0.683622),
+    'selu': (selu, 1., 0.),
+    'sigmoid': (tf.nn.sigmoid, 4., 0.5),
+    'sigmoid_num': (sigmoid_num, 2.751695, 1.4),
+    'softplus': (tf.nn.softplus, 2.0, 0.693147),
+    'softplus_num': (tf.nn.softplus, 1.843839, 1.021192),
+    'softsign': (tf.nn.softsign, 1., 0.),
+    'softsign_num': (softsign_num, 1.422959, 0.),
+    'tanh': (tf.nn.tanh, 1., 0.),
+    'tanh_num': (tanh_num, 1.147216, 0.),
+    'identity': ('identity', 1., 0.), }
+
+
+def get_activation(name):
+    if name in act_name_to_act_tuple:
+        return act_name_to_act_tuple[name]
+    else:
+        return None, None, None
