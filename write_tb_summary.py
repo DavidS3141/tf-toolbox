781e4c173f5e057a046037ba56c2568af7d12047
David Schmidt <david.schmidt@rwth-aachen.de>
2018-04-07 20:39:16 +0200

    add write_tb_summary

---
+++ write_tb_summary.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import tensorflow as tf
+
+
+def write_tb_summary(tb_saver, summary, global_step, values_dict):
+    s = tf.Summary()
+    for key in values_dict:
+        if values_dict[key] is not None:
+            s.value.add(tag=key, simple_value=float(values_dict[key]))
+    tb_saver.add_summary(s, global_step)
+    tb_saver.add_summary(summary, global_step)
