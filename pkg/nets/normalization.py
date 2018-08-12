5d5596f57c9b1c19eff28787ea8a8a2d9a148f26
David Schmidt <david.schmidt@rwth-aachen.de>
2018-08-12 18:01:34 +0200

    normalization handles more than just 2D shapes

--- pkg/nets/normalization.py
+++ pkg/nets/normalization.py
@@ -3,14 +3,12 @@ import tensorflow as tf
 
 
 def normalization(input_t, data_or_datalist, name='normalization'):
-    assert len(input_t.get_shape()) == 2
-    if type(data_or_datalist) is not list:
+    if isinstance(data_or_datalist, np.ndarray):
         datalist = [data_or_datalist]
     else:
         datalist = data_or_datalist
     for data in datalist:
-        assert len(data.shape) == 2
-        assert data.shape[1] == input_t.get_shape()[1]
+        assert data.shape[1:] == input_t.get_shape()[1:]
     data = np.concatenate(datalist, axis=0)
     mu = np.mean(data, axis=0, keepdims=True)
     std = np.std(data, axis=0, keepdims=True)
