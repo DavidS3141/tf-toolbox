f621e1787d3ae34e76937e18136955fa05140a2d
David Schmidt <david.schmidt@rwth-aachen.de>
2018-08-12 15:30:03 +0200

    trainer: add train loop function for standard trainer

--- trainer.py
+++ trainer.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 import numpy as np
 import os
 import shutil
@@ -30,7 +28,7 @@ class TF_Trainer(object):
         if isinstance(train_cfg, AttrDict) or isinstance(train_cfg, dict):
             self.train_cfg.update(train_cfg)
         else:
-            raise ValueError('train_cfg not valid config type')
+            raise TypeError('train_cfg not valid config type!')
         # #endregion train config
         # #region set random seeds
         if self.train_cfg.seed is not None:
@@ -125,8 +123,99 @@ class TF_Trainer(object):
         self.setup_infrastructure_training()
         self.train_loop()
 
+    def create_plots(self, plot_dir, **kwargs):
+        print('\t\tNothing to do here! Overwrite "create_plots" for action!')
+
     def train_loop(self):
-        raise NotImplementedError  # TODO implement default classifier
+        # #region loop variables
+        epoch = 0.
+        global_step = 0
+        nbr_readouts = 0
+        # #endregion loop variables
+
+        # #region initialize validation checker
+        validation_checker = Convergence_Checker(
+            min_iters=1, max_iters=np.inf,
+            min_confirmations=self.train_cfg.succ_validations)
+        # #endregion initialize validation checker
+
+        # #region main loop
+        with tqdm(total=self.train_cfg.max_epochs, unit='epoch',
+                  dynamic_ncols=True) as pbar:
+            while epoch < self.train_cfg.max_epochs:
+                pbar.update(int(epoch) - pbar.n)
+                # create batch
+                batch, epoch = next(self.train_queue)
+                global_step += 1
+                # #region training step
+                fd = self.get_feed_dict(batch)
+                (
+                    _,
+                    smaller_used_summary,
+                    bigger_used_summary,
+                ) = self.sess.run(
+                    [
+                        self.optimize_t,
+                        'smaller_used_summaries_t:0',
+                        'bigger_used_summaries_t:0',
+                    ],
+                    feed_dict=fd
+                )
+                # #endregion training step
+                # #region do validation
+                valid_batch, epoch_valid = next(self.valid_queue)
+                fd = self.get_feed_dict(valid_batch)
+                validation_value = self.sess.run(self.loss_t, feed_dict=fd)
+                validation_checker.check(validation_value)
+                # #endregion do validation
+                name = munge_filename(
+                    '%06d__%08.4f' % (global_step, 100.0 * epoch /
+                                      self.train_cfg.max_epochs))
+                # #region readout
+                if ((epoch / self.train_cfg.max_epochs >
+                     float(nbr_readouts) / self.train_cfg.nbr_readouts) or
+                        (epoch >= self.train_cfg.max_epochs) or
+                        validation_checker.is_converged()):
+                    nbr_readouts += 1
+                    print('\nThis is the %d readout ...' % nbr_readouts)
+                    print('\n\tCreate plots ...')
+                    plot_dir = os.path.join(self.plot_dir, name) + '/'
+                    os.makedirs(plot_dir)
+                    self.create_plots(plot_dir)
+                    print('\tPlots saved to %s!' % plot_dir)
+                    print('\n\tSave model ...')
+                    save_path = os.path.join(self.variables_dir, name)
+                    self.variables_saver.save(
+                        self.sess, save_path, global_step=global_step)
+                    print('\tModel saved in file: %s' % save_path)
+                    print('\n\tWrite tensorboard summary ...')
+                    write_tb_summary(
+                        self.tb_saver, bigger_used_summary, global_step,
+                        {
+                            'epoch': epoch,
+                            'validation': validation_value,
+                        }
+                    )
+                    print('\tFinished!')
+                    print('Finished readout!')
+                else:
+                    write_tb_summary(
+                        self.tb_saver, smaller_used_summary, global_step,
+                        {
+                            'epoch': epoch,
+                            'validation': validation_value,
+                        }
+                    )
+                # #endregion readout
+                # #region check validation for convergence and save best
+                if validation_checker.is_best(validation_value):
+                    save_path = os.path.join(self.best_dir, name)
+                    self.best_saver.save(self.sess, save_path,
+                                         global_step=global_step)
+                if validation_checker.is_converged():
+                    return
+                # #endregion check validation for convergence and save best
+        # #endregion main loop
 
 
 class Adversaries_Trainer(TF_Trainer):
@@ -160,15 +249,12 @@ class Adversaries_Trainer(TF_Trainer):
     def adversary_optimize_t(self):
         raise NotImplementedError
 
-    def create_plots(self, plot_dir, **kwargs):
-        print('\t\tNothing to do here! Overwrite "create_plots" for action!')
-
     def train_loop(self):
         # #region loop variables
         epoch = 0.
         global_step = 0
         turn = 'adversary'
-        performer_steps = 0
+        performer_step = 0
         nbr_readouts = 0
         # #endregion loop variables
 
@@ -215,7 +301,7 @@ class Adversaries_Trainer(TF_Trainer):
                     # #endregion train step for adversary
                 elif turn == 'performer':
                     # #region train step for performer
-                    performer_steps += 1
+                    performer_step += 1
                     (
                         _,
                         smaller_used_summary,
@@ -267,7 +353,7 @@ class Adversaries_Trainer(TF_Trainer):
                     validation_checker.check(validation_value)
                     # #endregion do validation
                     name = munge_filename(
-                        '%06d__%08.4f' % (performer_steps, 100.0 * epoch /
+                        '%06d__%08.4f' % (performer_step, 100.0 * epoch /
                                           self.train_cfg.max_epochs))
                     # #region readout
                     if ((epoch / self.train_cfg.max_epochs >
