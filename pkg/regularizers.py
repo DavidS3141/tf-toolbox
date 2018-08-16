78aaa61cc830b01489b558e0e5dd1794a032af8a
David Schmidt <david.schmidt@rwth-aachen.de>
2018-08-16 18:12:24 +0200

    do the weight decay in a normalized manner

--- pkg/regularizers.py
+++ pkg/regularizers.py
@@ -1,8 +1,12 @@
 import tensorflow as tf
 
 
-def weight_decay_regularizer(dependency_ops, var_list, weight_decay=0.0005):
+def weight_decay_regularizer(dependency_ops, total_number_iterations,
+                             var_list=None, normalized_weight_decay=0.05):
     weights = tf.get_collection(tf.GraphKeys.WEIGHTS)
+    if not var_list:
+        var_list = weights
+    weight_decay = normalized_weight_decay * (1. / total_number_iterations)**0.5
     decay_param = 1. - weight_decay
     with tf.control_dependencies(dependency_ops):
         return tf.group(*[tf.assign(var, var * decay_param)
