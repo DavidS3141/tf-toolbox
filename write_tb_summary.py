209c42f109416a2e641f22bf0c18b58dbd02b163
David Schmidt <david.schmidt@rwth-aachen.de>
2018-08-17 21:20:58 +0200

    write_tb_summary accepts as summary a protobuf or dict object

--- write_tb_summary.py
+++ write_tb_summary.py
@@ -3,8 +3,11 @@ import tensorflow as tf
 
 def write_tb_summary(tb_saver, summary, global_step, values_dict):
     s = tf.Summary()
+    if isinstance(summary, str):
+        tb_saver.add_summary(summary, global_step)
+    elif isinstance(summary, dict):
+        values_dict.update(summary)
     for key in values_dict:
         if values_dict[key] is not None:
             s.value.add(tag=key, simple_value=float(values_dict[key]))
-    tb_saver.add_summary(s, global_step)
-    tb_saver.add_summary(summary, global_step)
+            tb_saver.add_summary(s, global_step)
