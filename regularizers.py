8e99de047521839ec3b6c9c7e0707d8c6888b17d
David Schmidt <david.schmidt@rwth-aachen.de>
2018-09-03 12:09:47 +0200

    update regularizer respecting scheduling

--- regularizers.py
+++ regularizers.py
@@ -2,11 +2,17 @@ import tensorflow as tf
 
 
 def weight_decay_regularizer(dependency_ops, total_number_iterations,
-                             var_list=None, normalized_weight_decay=0.05):
+                             scheduler=tf.constant(1.), var_list=None,
+                             normalized_weight_decay=0.05):
+    '''
+    https://arxiv.org/pdf/1711.05101.pdf
+    '''
     weights = tf.get_collection(tf.GraphKeys.WEIGHTS)
     if not var_list:
         var_list = weights
-    weight_decay = normalized_weight_decay * (1. / total_number_iterations)**0.5
+    weight_decay = normalized_weight_decay \
+        * (1. / total_number_iterations)**0.5 \
+        * scheduler
     decay_param = 1. - weight_decay
     with tf.control_dependencies(dependency_ops):
         return tf.group(*[tf.assign(var, var * decay_param)
