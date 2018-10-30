200f310db076e10543c8d1297cec5d8d0a7e1a10
David Schmidt <david.schmidt@rwth-aachen.de>
2018-10-30 16:29:11 +0100

    regularizer: exponential decay that is turned off (param=0) not in graph

--- regularizers.py
+++ regularizers.py
@@ -7,13 +7,17 @@ def weight_decay_regularizer(dependency_ops, total_number_iterations,
     '''
     https://arxiv.org/pdf/1711.05101.pdf
     '''
-    weights = tf.get_collection(tf.GraphKeys.WEIGHTS)
-    if not var_list:
-        var_list = weights
-    weight_decay = normalized_weight_decay \
-        * (1. / total_number_iterations)**0.5 \
-        * scheduler
-    decay_param = 1. - weight_decay
-    with tf.control_dependencies(dependency_ops):
-        return tf.group(*[tf.assign(var, var * decay_param)
-                          for var in var_list if var in weights])
+    if normalized_weight_decay == 0.0:
+        with tf.control_dependencies(dependency_ops):
+            return tf.group(*dependency_ops)
+    else:
+        weights = tf.get_collection(tf.GraphKeys.WEIGHTS)
+        if not var_list:
+            var_list = weights
+        weight_decay = normalized_weight_decay \
+            * (1. / total_number_iterations)**0.5 \
+            * scheduler
+        decay_param = 1. - weight_decay
+        with tf.control_dependencies(dependency_ops):
+            return tf.group(*[tf.assign(var, var * decay_param)
+                              for var in var_list if var in weights])
